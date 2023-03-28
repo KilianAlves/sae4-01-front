e6b541fe895e14f6c93d87caf44937879ec137b7
KilianAlves <alveskilian@gmail.com>
2023-03-28 23:04:17 +0200

    ajout de fetchAllQuestion

--- src/services/api/api.js
+++ src/services/api/api.js
@@ -1,1 +1,7 @@
 export const BASE_URL = 'https://127.0.0.1:8000/api';
+
+export function fetchAllQuestions() {
+    return fetch(`${BASE_URL}/bookmarks`)
+      .then((response) => response.json());
+  }
+  
