9d1a9c67b10fa2c6b5b39c3748bdf7ac1aab2ed7
KilianAlves <alveskilian@gmail.com>
2023-04-03 20:50:10 +0200

    Composant QuestionItem fonctionnelle

--- src/components/QuestionItem.jsx
+++ src/components/QuestionItem.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import PropTypes from 'prop-types';
+import { getUserInfo } from "../services/api/api";
 
 export default function QuestionItem({data}) {
+    
+    let utilisateur = data.utilisateur;
+    let infoUser = getUserInfo(data.utilisateur);
+
     return (
         <div class="Question">
             <a href={data.id}>{data.titre}</a>
-            <p>ecrite par {data.utilisateur.nom} {data.utilisateur.prenom} le {data.dateEcriture}</p>
+            <p>ecrite par {infoUser.nom} {infoUser.prenom} le {data.dateEcriture}</p>
         </div>
     );
 }
