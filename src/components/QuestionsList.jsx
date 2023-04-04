5efe6820d49255c53bcb79b468e328559e6199fd
KilianAlves <alveskilian@gmail.com>
2023-04-04 22:23:12 +0200

    Ajout d'un useEffect

--- src/components/QuestionsList.jsx
+++ src/components/QuestionsList.jsx
@@ -1,29 +1,30 @@
-import React, {useState, useEffect, useCallback} from "react";
+import React, { useState, useEffect } from "react";
 import { fetchAllQuestions } from "../services/api/api";
 import QuestionItem from "./QuestionItem";
 import Loading from "./Loading";
 
 export default function QuestionsList() {
+  const [questionsList, setQuestionsList] = useState(null);
+  useEffect(() => {
+    fetchAllQuestions().then((data) => {
+      // setBookmarksData(data["hydra:member"]);
+      setQuestionsList(
+        data["hydra:member"].map((question) => (
+          <QuestionItem key={question.id} data={question} />
+        ))
+      );
+    });
+  }, []);
 
-    const [questionsList, setQuestionsList] = useState(null);
-    fetchAllQuestions().then((data) =>
-        {
-            //setBookmarksData(data["hydra:member"]);
-            setQuestionsList(data["hydra:member"].map((question) =>
-                <QuestionItem key={question.id} data={question}/>
-            ));
-        });
+  // si pas de donnée ou chargement
+  if (questionsList === null) {
+    return <Loading />;
+  }
 
-
-    //si pas de donnée ou chargement
-    if (questionsList === null) {
-        return <Loading/>;
-    }
-
-    return (
-        <div>
-            <h1>Liste de questions</h1>
-            <div>{questionsList}</div>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Liste de questions</h1>
+      <div>{questionsList}</div>
+    </div>
+  );
 }
