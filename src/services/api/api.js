7f50892d5fec318c3f022f4076ffe6c32bd3e106
KilianAlves <alveskilian@gmail.com>
2023-04-04 22:13:03 +0200

    Ajout de fetchQuestionById

--- src/services/api/api.js
+++ src/services/api/api.js
@@ -1,7 +1,23 @@
-export const BASE_URL = 'https://127.0.0.1:8000/api';
+export const BASE_URL = "https://127.0.0.1:8000/api";
 
 export function fetchAllQuestions() {
-    return fetch(`${BASE_URL}/bookmarks`)
-      .then((response) => response.json());
-  }
-  
+  return fetch(`${BASE_URL}/questions`).then((response) => response.json());
+}
+
+export function getUserInfo(user) {
+  return fetch(`https://127.0.0.1:8000${user}`).then((response) =>
+    response.json()
+  );
+}
+
+export function fetchReponsesByQuestionId(questionId) {
+  return fetch(`${BASE_URL}/reponses?question=${questionId}`).then((response) =>
+    response.json()
+  );
+}
+
+export function fetchQuestionById(id) {
+  return fetch(`${BASE_URL}/questions/${id}`).then((response) =>
+    response.json()
+  );
+}
