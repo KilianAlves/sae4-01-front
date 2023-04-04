ae10661ed3e8e5ac554216643188c033710cfb00
KilianAlves <alveskilian@gmail.com>
2023-04-04 22:25:01 +0200

    fixe de l'erreur avec vite dans app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import viteLogo from "../../../../../../../../vite.svg";
 import "./App.css";
 import QuestionsList from "./components/QuestionsList";
 import QuestionItem from "./components/QuestionItem";
