e1017a4f6a185d75e755aaa690758a88e2c27311
KilianAlves <alveskilian@gmail.com>
2023-04-04 22:20:56 +0200

    Modification du composant QuestionItem pour changer de question avec wouter

--- src/components/QuestionItem.jsx
+++ src/components/QuestionItem.jsx
@@ -1,16 +1,19 @@
 import React from "react";
-import PropTypes from 'prop-types';
-import { getUserInfo } from "../services/api/api";
+import { useLocation } from "wouter";
 
-export default function QuestionItem({data}) {
-    
-    let utilisateur = data.utilisateur;
-    let infoUser = getUserInfo(data.utilisateur);
-
-    return (
-        <div class="Question">
-            <a href={data.id}>{data.titre}</a>
-            <p>ecrite par {infoUser.nom} {infoUser.prenom} le {data.dateEcriture}</p>
-        </div>
-    );
+export default function QuestionItem({ data }) {
+  // let utilisateur = data.utilisateur;
+  // let infoUser = getUserInfo(data.utilisateur);
+  const [location, setLocation] = useLocation();
+  const questionClick = () => {
+    // Rediriger vers la page question correspondante
+    setLocation(`/questions/${data.id}`);
+  };
+  // par {infoUser.nom} {infoUser.prenom}
+  return (
+    <div className="Question" onClick={questionClick}>
+      <div>{data.titre}</div>
+      <p>écrite le {data.dateEcriture}</p>
+    </div>
+  );
 }
