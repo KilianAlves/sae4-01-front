45a40d09e9a0a8905efde1148f9a291721c3dee6
KilianAlves <alveskilian@gmail.com>
2023-04-06 01:00:26 +0200

    Ajout d'une route de creation de question

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import QuestionsList from "./components/QuestionsList";
 import UserButton from "./components/UserButton";
 import QuestionEtReponses from "./components/QuestionEtReponses";
+import QuestionForm from "./components/QuestionForm";
 import { Router, Route } from "wouter";
 import { default as UserProvider } from './contexts/user/Provider';
 
@@ -15,6 +16,7 @@ function App() {
       <Router>
         <Route path="/questions/" component={QuestionsList} />
         <Route path="/questions/:id" component={QuestionEtReponses} />
+        <Route path="/create/question" component={QuestionForm} />
       </Router>
     </UserProvider>
   );
