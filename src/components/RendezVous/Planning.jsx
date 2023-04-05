fa39023cd5f2a4c5f9ec1dc417d4235259655898
Viot Baptiste <baptiste.viot@etudiant.univ-reims.fr>
2023-04-05 23:32:28 +0200

    Création du composant Planning

--- src/components/RendezVous/Planning.jsx
+++ src/components/RendezVous/Planning.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 import Jour from "./Jour.jsx";
 import {getRdvSemaine} from "../../services/api/rendezvous.js";
+import {useEffect, useState} from "react";
+import Loading from "../Loading.jsx";
 
-export default async function Planning() {
-    const listRdv = await getRdvSemaine();
-    const rendu = listRdv.map((creneaux, key) =>
-            <div>
-                <div>{ key }</div>
-                <Jour creneaux={creneaux}/>
-            </div>
-        )
-    return (
-        {rendu}
-    )
+export default function Planning() {
+    const [rdvList, setRdvList] = useState();
+
+    useEffect(() => {
+        getRdvSemaine().then((data) => {
+            setRdvList(Object.entries(data).map(([key, creneaux]) => (
+                <div key={key}>
+                    <div>{ key }</div>
+                    <Jour key={key} date={key} creneaux={Object.values(creneaux)}/>
+                </div>
+            )))
+        });
+    }, []);
+
+    if (!rdvList) {
+        return <Loading />;
+    }
+    return rdvList
+
 }
