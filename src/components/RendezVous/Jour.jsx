d7f61f79a74884a51833afbba44f00d965460cc6
Viot Baptiste <baptiste.viot@etudiant.univ-reims.fr>
2023-04-05 23:32:18 +0200

    Création du composant Jour

--- src/components/RendezVous/Jour.jsx
+++ src/components/RendezVous/Jour.jsx
@@ -1,13 +1,11 @@
 import React from "react";
+import {postRdv} from "../../services/api/rendezvous.js";
 
-export default function Jour({creneaux}) {
-    const rendu = creneaux.map((creneau) =>
-        <div>
-            <button>{creneau}</button>
-        </div>
-        //onClick="rdv('{{ key }}', {{ creneau }})"
-    )
-    return (
-        {rendu}
-    )
+export default function Jour({creneaux, date}) {
+  const veterinaire = 12;
+  const rendu = creneaux.map((creneau) => (
+    <button key={date+creneau} onClick={() => postRdv(date, creneau,veterinaire )}>{creneau}</button>
+    //onClick="rdv('{{ key }}', {{ creneau }})"
+  ))
+  return <div>{rendu}</div>
 }
