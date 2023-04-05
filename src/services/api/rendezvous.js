9e08fcbaa7aec05348b06bcca3d7d383b5d4f544
Viot Baptiste <baptiste.viot@etudiant.univ-reims.fr>
2023-04-05 23:36:51 +0200

    Création de la requête postRdv

--- src/services/api/rendezvous.js
+++ src/services/api/rendezvous.js
@@ -1,7 +1,24 @@
-export const BASE_URL = "https://127.0.0.1:8000/api/rendezvous";
+export const BASE_URL = "http://127.0.0.1:8000/api/rendez_vouse";
 
 export function getRdvSemaine(semaine) {
     return fetch(`${BASE_URL}/semaine`).then((response) => response.json());
 }
 
-
+export function postRdv(date, horaire, veterinaire) {
+    const body = {
+        dateRdv: date,
+        horaire: horaire,
+        veterinaire: "api/veterinaires/"+veterinaire,
+        motif: " ",
+        estUrgent: 1,
+        commentaireVeto: " ",
+        estDomicile: 1,
+    }
+    return fetch(`${BASE_URL}s`, {
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        method: "POST",
+        body: JSON.stringify(body)}).then((response) => response.json());
+}
