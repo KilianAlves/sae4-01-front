46f4f3633b71ffd4a8eaef843c86d4bc086b982e
Viot Baptiste <baptiste.viot@etudiant.univ-reims.fr>
2023-04-05 23:36:20 +0200

    Modif du routeur

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,18 @@ import QuestionsList from "./components/QuestionsList";
 import QuestionItem from "./components/QuestionItem";
 import QuestionEtReponses from "./components/QuestionEtReponses";
 import { Router, Route } from "wouter";
+import Planning from "./components/RendezVous/Planning.jsx";
+import Accueil from "./components/Accueil/Accueil.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <Router>
-      <Route path="/questions/" component={QuestionsList} />
+      <Route path="/" component={Accueil} />
+      <Route path="/questions" component={QuestionsList} />
       <Route path="/questions/:id" component={QuestionEtReponses} />
+      <Route path="/rendezvous" component={Planning} />
     </Router>
   );
 }
