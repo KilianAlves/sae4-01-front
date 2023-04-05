bdfe50601fd889e741058a2d29fbbdfa5b63d490
Viot Baptiste <baptiste.viot@etudiant.univ-reims.fr>
2023-04-06 00:50:59 +0200

    Fix du composant Jour

--- src/components/RendezVous/Jour.jsx
+++ src/components/RendezVous/Jour.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import {postRdv} from "../../services/api/rendezvous.js";
 
-export default function Jour({creneaux, date}) {
-  const veterinaire = 12;
+export default function Jour({creneaux, date, veterinaire}) {
   const rendu = creneaux.map((creneau) => (
     <button key={date+creneau} onClick={() => postRdv(date, creneau,veterinaire )}>{creneau}</button>
     //onClick="rdv('{{ key }}', {{ creneau }})"
