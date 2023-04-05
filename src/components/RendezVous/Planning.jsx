23fa1646fd8635bc3cb6b3594662b19d4b5ed724
Viot Baptiste <baptiste.viot@etudiant.univ-reims.fr>
2023-04-06 00:51:18 +0200

    Fix du composant Planning

--- src/components/RendezVous/Planning.jsx
+++ src/components/RendezVous/Planning.jsx
@@ -3,16 +3,19 @@ import Jour from "./Jour.jsx";
 import {getRdvSemaine} from "../../services/api/rendezvous.js";
 import {useEffect, useState} from "react";
 import Loading from "../Loading.jsx";
+import {useRoute} from "wouter";
 
 export default function Planning() {
+    const [_, params] = useRoute("/rendezvous/:id");
+    const { id } = params;
     const [rdvList, setRdvList] = useState();
 
     useEffect(() => {
-        getRdvSemaine().then((data) => {
+        getRdvSemaine("", id).then((data) => {
             setRdvList(Object.entries(data).map(([key, creneaux]) => (
                 <div key={key}>
                     <div>{ key }</div>
-                    <Jour key={key} date={key} creneaux={Object.values(creneaux)}/>
+                    <Jour key={key} date={key} veterinaire={id} creneaux={Object.values(creneaux)}/>
                 </div>
             )))
         });
