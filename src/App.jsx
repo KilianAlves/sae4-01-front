38877ecfe252ed4b58a7e7c09b20404e74825723
Viot Baptiste <baptiste.viot@etudiant.univ-reims.fr>
2023-04-06 00:49:09 +0200

    Ajout d'une route dans le routeur

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { Router, Route } from "wouter";
 import { default as UserProvider } from './contexts/user/Provider';
 import Planning from "./components/RendezVous/Planning.jsx";
 import Accueil from "./components/Accueil/Accueil.jsx";
+import VeterinaireList from "./components/RendezVous/VeterinaireList.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -18,7 +19,8 @@ function App() {
         <Route path="/" component={Accueil} />
         <Route path="/questions/" component={QuestionsList} />
         <Route path="/questions/:id" component={QuestionEtReponses} />
-        <Route path="/rendezvous" component={Planning} />
+        <Route path="/rendezvous" component={VeterinaireList} />
+        <Route path="/rendezvous/:id" component={Planning} />
       </Router>
     </UserProvider>
   );
