9fd9696782e69d53564bf7ab8f7eb7ef2f656b96
KilianAlves <alveskilian@gmail.com>
2023-04-06 02:16:26 +0200

    ajout d'une route pour les ajout de reponse

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import QuestionsList from "./components/QuestionsList";
 import UserButton from "./components/UserButton";
 import QuestionEtReponses from "./components/QuestionEtReponses";
 import QuestionForm from "./components/QuestionForm";
+import ReponseForm from "./components/ReponseForm";
 import { Router, Route } from "wouter";
 import { default as UserProvider } from './contexts/user/Provider';
 
@@ -17,6 +18,7 @@ function App() {
         <Route path="/questions/" component={QuestionsList} />
         <Route path="/questions/:id" component={QuestionEtReponses} />
         <Route path="/create/question" component={QuestionForm} />
+        <Route path="/create/reponse/:id" component={ReponseForm} />
       </Router>
     </UserProvider>
   );
