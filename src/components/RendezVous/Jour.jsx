5d0169ff26947b31f912c2641fa5a28122ab858c
Viot Baptiste <baptiste.viot@etudiant.univ-reims.fr>
2023-04-08 15:59:20 +0200

    RDV: Ajout demande de confirmation prise RDV

--- src/components/RendezVous/Jour.jsx
+++ src/components/RendezVous/Jour.jsx
@@ -1,9 +1,17 @@
 import React from "react";
-import {postRdv} from "../../services/api/rendezvous.js";
+import {ConfirmDialog, confirmDialog} from "primereact/confirmdialog";
 
-export default function Jour({creneaux, date, veterinaire}) {
+export default function Jour({creneaux, date, veterinaire, createRDV}) {
+  const confirm = (creneau) => {
+    confirmDialog({
+      message: 'Vous êtes sur ?',
+      header: 'Confirmation',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => createRDV(date, creneau, veterinaire),
+    });
+  };
   const rendu = creneaux.map((creneau) => (
-    <button key={date+creneau} onClick={() => postRdv(date, creneau,veterinaire )}>{creneau}</button>
+    <button key={date+creneau} onClick={() => {confirm(creneau)}}>{creneau}</button>
     //onClick="rdv('{{ key }}', {{ creneau }})"
   ))
   return <div>{rendu}</div>
