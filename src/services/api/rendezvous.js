03c2d1544ac170f32789a476e4337b1084aac015
Viot Baptiste <baptiste.viot@etudiant.univ-reims.fr>
2023-04-08 15:48:34 +0200

    Fixe services (session & https)

--- src/services/api/rendezvous.js
+++ src/services/api/rendezvous.js
@@ -1,25 +1,30 @@
-export const BASE_URL = "http://127.0.0.1:8000/api/rendez_vouse";
+export const BASE_URL = "https://127.0.0.1:8000/api/rendez_vouse";
 
 export function getRdvSemaine(semaine, veterinaireId) {
-    return fetch(`${BASE_URL}/semaine/${veterinaireId}?date=2023-06-05`).then((response) => response.json());
+  return fetch(`${BASE_URL}/semaine/${veterinaireId}?date=2023-06-05`, {
+    credentials: "include",
+  }).then((response) => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw Error(response.statusText);
+  });
 }
 
 export function postRdv(date, horaire, veterinaire) {
-    const body = {
-        dateRdv: date,
-        horaire: horaire,
-        veterinaire: "api/veterinaires/"+veterinaire,
-        motif: " ",
-        estUrgent: 1,
-        commentaireVeto: " ",
-        estDomicile: 1,
-        client: "api/clients/1",
-    }
-    return fetch(`${BASE_URL}s`, {
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        method: "POST",
-        body: JSON.stringify(body)}).then((response) => response.json());
+  const body = {
+    dateRdv: date,
+    horaire,
+    veterinaire: `api/veterinaires/${veterinaire}`,
+    motif: " ",
+  };
+  return fetch(`${BASE_URL}s`, {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+    method: "POST",
+    body: JSON.stringify(body),
+  }).then((response) => response.json());
 }
